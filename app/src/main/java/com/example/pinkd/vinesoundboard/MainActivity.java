2364d95952cee0daff0120fe9e6f14233deaaad0
Jacqueline Osborn <jackieo5023@gmail.com>
2017-12-07 16:56:24 -0600

    Added button with sound

---
+++ app/src/main/java/com/example/pinkd/vinesoundboard/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.pinkd.vinesoundboard;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button button = (Button) findViewById(R.id.button_id);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                playSound(R.raw.shower);
+            }
+        });
+    }
+
+    public void playSound(int sound) {
+        MediaPlayer mp = MediaPlayer.create(this, sound);
+        mp.start();
+    }
+}
